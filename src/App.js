c329dc62670b80133f55ff783d5d3e10bc16fb28
unknown <mathapationkar8@gmail.com>
2022-01-17 21:49:43 +0530

    base folder structure created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import ExpenseItem from './components/ExpenseItems';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app() {
+          return (
+                    <div>
+                         <h2>Lets Go!</h2>
+                         <ExpenseItem></ExpenseItem>
+                    </div>
+          )
 }
 
-export default App;
+export default app;
---
+++ src/components/ExpenseItems.jsx
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+          return(
+               <>
+               <div>March 28th 2021</div>
+               <div>
+                  <h2>Car Insurance</h2>
+                  <div>$294.3</div>
+               </div>
+               </>     
+          )
+}
+
+export default ExpenseItem;
